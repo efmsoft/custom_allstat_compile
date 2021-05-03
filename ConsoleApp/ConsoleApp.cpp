954c52643b53dd58172502a3fe04d2703711db24
Eduard Mishkurov <emishkurov@gmail.com>
2021-05-03 10:26:39 +0300

    print list of generator modules

--- ConsoleApp/ConsoleApp.cpp
+++ ConsoleApp/ConsoleApp.cpp
@@ -1,4 +1,27 @@
+#include <AllStat/AllStat.h>
+
+#include <map>
+#include <stdio.h>
+
 int main()
 {
+  AS_ITEM item;
+  AS_ENUM_CONTEXT context;
+  if (AllStatGetFirst(AS_GENERATOR::AS_ANY, &context, &item) == AS_SUCCESS)
+  {
+    std::map<AS_GENERATOR, bool> stat;
+    do
+    {
+      stat[item.Generator] = true;
+
+    } while (AllStatGetNext(&context, &item) == AS_SUCCESS);
+
+    printf("List of generator modules:\n");
+    for (auto it = stat.begin(); it != stat.end(); ++it)
+    {
+      std::string name = AllStat::GetGenerator(it->first);
+      printf(" %s\n", name.c_str());
+    }
+  }
   return 0;
 }
